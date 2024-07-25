54323b0423d858e9095ec034d0ca3d1c9d3b0362
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-07-25 09:27:48 +0000

    added a lambda function

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ window = tk.Tk()
 window.title("Hello world")
 window.geometry("300x300")
 
+def fun(name):
+  print("Hello", name)
+
 hello = tk.Label(text="Hello world!")
 hello.pack()
-button = tk.Button(text="Click me!")
+button = tk.Button(text="Click me!", command=lambda: fun("derrick"))
 button.pack()
 
 tk.mainloop()
